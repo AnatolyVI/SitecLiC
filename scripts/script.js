f3d9399a27eda8997dbfefa57c833f7686e98632
AnatolyVI <anatoly.iv.pavlov@gmail.com>
2024-09-07 02:10:47 +0500

    Script gor BGM

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,13 @@
 "use strict"
-document.addEventListener('click', musicPlay);
-function musicPlay() {
-    document.getElementById('LamentBGM').play();
-    document.removeEventListener('click', musicPlay);
+
+// Создаем объект Audio и указываем путь к аудиофайлу
+const audio = new Audio('../sounds/Limbus Company - In Hell We Live, Lament (Instrumental).mp3');
+
+let playing = 1
+// Функция для воспроизведения аудио
+function playAudio() {
+    audio.volume = 0.1;  // Устанавливаем громкость на максимум
+    audio.play();
 }
+// Воспроизводим аудио автоматически при загрузке страницы или по какому-то событию
+playAudio();
